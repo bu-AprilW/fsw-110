74897ce892fda6e5c0024e1c7e423b4d87877bb5
https://github.com/bu-AprilW/fsw-110.git <april.william@bryanuniversity.edu>
2021-01-26 20:33:58 -0600

    project1

--- week2/basic_website_wJS/index.js
+++ week2/basic_website_wJS/index.js
@@ -1,5 +1,9 @@
 var navbar = document.createElement("nav");
-document.append(navbar);
+var node = document.createElement("a");
+var home = document.createTextNode("Home");
+node.appendChild(home);
+navbar.appendChild(node);
+document.body.append(navbar);
 
 var header = document.createElement("h1");
 var text = document.createTextNode("Project 1");
@@ -12,11 +16,24 @@ document.body.append(par);
 
 var list = document.createElement("ol");
 var li = document.createElement("li");
-let items = ['One', 'Two', 'Three'];
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
 
-li.append(items);
+let item = ["one"];
+li.append(item);
 list.append(li);
+
+let item2 = ["two"];
+li2.append(item2);
+list.append(li2);
+
+let item3 = ["three"];
+li3.append(item3);
+list.append(li3);
+
 document.body.append(list);
 
 var footer = document.createElement("footer");
+var author = document.createTextNode("By: April Williams");
+footer.appendChild(author);
 document.body.append(footer);
