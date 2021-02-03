b8e7857b2464c33dbd5058ecc89694020ac09b1b
https://github.com/bu-AprilW/fsw-110.git <april.william@bryanuniversity.edu>
2021-02-03 08:24:45 -0600

    first edit

--- week6/events_workbook/click/app.js
+++ week6/events_workbook/click/app.js
@@ -1,1 +1,4 @@
-//make the box disapear when the user clicks it
+//make the box disappear when the user clicks it
+document.getElementsByClassName("red-box")[0].addEventListener("click", function(e) {
+    document.getElementsByClassName("red-box")[0].style.display = "none";
+});
