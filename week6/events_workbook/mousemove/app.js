033964e6053c048f3fec3f0e27246aa881e398d4
https://github.com/bu-AprilW/fsw-110.git <april.william@bryanuniversity.edu>
2021-02-03 22:08:12 -0600

    mousemove

---
+++ week6/events_workbook/mousemove/app.js
@@ -0,0 +1,17 @@
+var ele = document.getElementsByClassName("red-box");
+ele[0].addEventListener("mousemove", showCords);
+
+function showCords(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var cord = "X cords: " + x +", Y cords:" + y;
+ele[0].innerHTML = cord;
+}
+
+
+document.getElementsByClassName("red-box");
+ele[0].addEventListener("mouseout", clearCord);
+
+function clearCord() {
+    ele[0].innerHTML = "";
+}
