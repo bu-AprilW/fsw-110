93ed9fa895949aa1a3f20ed6d7a0abbc44baaf18
https://github.com/bu-AprilW/fsw-110.git <april.william@bryanuniversity.edu>
2021-02-03 22:34:12 -0600

    submit

--- week6/events_workbook/submit/app.js
+++ week6/events_workbook/submit/app.js
@@ -1,3 +1,8 @@
 // The "submit" event refreshes your browser by default
 // use "e.preventDefault()" in the first line of your callback function to prevent this
 // remember to pass "e" into the callback function
+
+var s = document.getElementById("submit-me");
+s.addEventListener("click", function(e) {
+    e.preventDefault()
+});
